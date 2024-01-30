4e97ae4383d562074fc645601953c23a8ca9fe73
zopalmer14 <zopalmer14@gmail.com>
2024-01-29 19:43:18 -0500

    create basic math functions

--- dynamic.js
+++ dynamic.js
@@ -1,4 +1,22 @@
 
+// MATH FUNCTIONS
+
+function add(operand_1, operand_2) {
+    return operand_1 + operand_2;
+}
+
+function subtract(operand_1, operand_2) {
+    return operand_1 - operand_2;
+}
+
+function multiply(operand_1, operand_2) {
+    return operand_1 * operand_2;
+}
+
+function divide(operand_1, operand_2) {
+    return operand_1 / operand_2;
+}
+
 // USEFUL DOM REFERENCES
 
 const grid_container = document.querySelector('#grid_container');
